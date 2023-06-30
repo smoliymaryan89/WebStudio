146a6216ea375c9396ccab76a34e79a298966f4e
maryan <smoliymaryan89@gmail.com>
2023-06-30 13:24:49 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -3,10 +3,13 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.backdrop'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.backdrop.addEventListener('click', handleBackdropClick);
+  document.addEventListener('keydown', handleEscKey);
 
   function toggleModal() {
     const isModalHidden = refs.modal.classList.contains('is-hidden');
@@ -17,6 +20,18 @@
     } else {
       refs.modal.classList.add('is-hidden');
       document.body.style.overflow = '';
+    }
+  }
+
+  function handleBackdropClick(event) {
+    if (event.target === refs.backdrop) {
+      toggleModal();
+    }
+  }
+
+  function handleEscKey(event) {
+    if (event.key === 'Escape') {
+      toggleModal();
     }
   }
 })();
